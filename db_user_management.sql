91600143f0b644097f7fcce1b5e9c7311a1adc62
Dikki Frana Alvian <dikkifranaalvian@gmail.com>
2026-06-14 06:30:22 +0700

    update db

--- db_user_management.sql
+++ db_user_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 13, 2026 at 04:37 PM
+-- Generation Time: Jun 14, 2026 at 01:29 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -53,17 +53,42 @@ INSERT INTO `log_activity` (`id`, `user_id`, `username`, `aktivitas`, `waktu`) V
 (12, 6, 'admin', 'Ubah Status: admin (Admin → Admin)', '2026-06-13 14:13:39'),
 (13, 6, 'admin', 'Ubah Status: admin (Admin → Admin)', '2026-06-13 14:13:39'),
 (14, 6, 'admin', 'Logout', '2026-06-13 14:23:39'),
-(15, 7, 'dikki', 'Register', '2026-06-13 14:25:17'),
-(16, 7, 'dikki', 'Login', '2026-06-13 14:25:29'),
-(17, 7, 'dikki', 'Logout', '2026-06-13 14:25:48'),
+(15, NULL, 'dikki', 'Register', '2026-06-13 14:25:17'),
+(16, NULL, 'dikki', 'Login', '2026-06-13 14:25:29'),
+(17, NULL, 'dikki', 'Logout', '2026-06-13 14:25:48'),
 (18, 6, 'admin', 'Login', '2026-06-13 14:25:54'),
 (19, 6, 'admin', 'Ubah Status: dikki (User → Admin)', '2026-06-13 14:26:05'),
 (20, 6, 'admin', 'Logout', '2026-06-13 14:26:11'),
-(21, 7, 'dikki', 'Login', '2026-06-13 14:26:19'),
-(22, 7, 'dikki', 'Logout', '2026-06-13 14:26:38'),
+(21, NULL, 'dikki', 'Login', '2026-06-13 14:26:19'),
+(22, NULL, 'dikki', 'Logout', '2026-06-13 14:26:38'),
 (23, 6, 'admin', 'Login', '2026-06-13 14:27:08'),
 (24, 6, 'admin', 'Ubah Status: dikki (Admin → User)', '2026-06-13 14:27:22'),
-(25, 6, 'admin', 'Logout', '2026-06-13 14:27:25');
+(25, 6, 'admin', 'Logout', '2026-06-13 14:27:25'),
+(26, 6, 'admin', 'Login', '2026-06-13 22:54:22'),
+(27, 6, 'admin', 'Logout', '2026-06-13 22:55:25'),
+(28, 8, 'frana', 'Register', '2026-06-13 22:55:51'),
+(29, 8, 'frana', 'Login', '2026-06-13 22:56:06'),
+(30, 8, 'frana', 'Logout', '2026-06-13 22:59:40'),
+(31, NULL, 'dikki', 'Login', '2026-06-13 23:00:00'),
+(32, NULL, 'dikki', 'Logout', '2026-06-13 23:00:14'),
+(33, 6, 'admin', 'Login', '2026-06-13 23:00:23'),
+(34, 6, 'admin', 'Ubah Status: frana (User → Admin)', '2026-06-13 23:00:40'),
+(35, 6, 'admin', 'Logout', '2026-06-13 23:00:49'),
+(36, 8, 'frana', 'Login', '2026-06-13 23:00:58'),
+(37, 8, 'frana', 'Logout', '2026-06-13 23:02:20'),
+(38, NULL, 'dikki', 'Login', '2026-06-13 23:02:34'),
+(39, NULL, 'dikki', 'Logout', '2026-06-13 23:02:39'),
+(40, 8, 'frana', 'Login', '2026-06-13 23:02:49'),
+(41, 8, 'frana', 'Reset Password: dikki', '2026-06-13 23:03:12'),
+(42, 8, 'frana', 'Logout', '2026-06-13 23:03:16'),
+(43, NULL, 'dikki', 'Login', '2026-06-13 23:03:40'),
+(44, NULL, 'dikki', 'Logout', '2026-06-13 23:03:51'),
+(45, 8, 'frana', 'Login', '2026-06-13 23:03:57'),
+(46, 8, 'frana', 'Hapus User: dikki', '2026-06-13 23:04:18'),
+(47, 8, 'frana', 'Logout', '2026-06-13 23:04:32'),
+(48, 6, 'admin', 'Login', '2026-06-13 23:04:42'),
+(49, 6, 'admin', 'Ubah Status: frana (Admin → User)', '2026-06-13 23:04:59'),
+(50, 6, 'admin', 'Logout', '2026-06-13 23:06:37');
 
 -- --------------------------------------------------------
 
@@ -85,7 +110,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`, `status`, `created_at`) VALUES
 (6, 'admin', '$2y$10$tWPWNu/U5njyMKiUsHgOc.0yUv7VkmBwqunquHdN5mm3M9xlN1nrG', 'Admin', '2026-06-13 10:58:01'),
-(7, 'dikki', '$2y$10$RWvJBoj47BTAOr10QzKEC..yVCPyItw5je4x5wPeIqOadGdZXvJAy', 'User', '2026-06-13 14:25:17');
+(8, 'frana', '$2y$10$I7K1c.clIzM6G2sY38YIye/Nfxtc0mzm83tnOsA9JglXhqdzkPnPe', 'User', '2026-06-13 22:55:51');
 
 --
 -- Indexes for dumped tables
@@ -113,13 +138,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `log_activity`
 --
 ALTER TABLE `log_activity`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Constraints for dumped tables
